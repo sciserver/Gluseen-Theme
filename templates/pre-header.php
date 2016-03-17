d4260a388d40439e983a1c3b84174c6c8b9c4601
bonbons0220 <bsouter@jhu.edu>
2016-03-17 06:52:25 -0400

    added pre-header with google tag manager code

---
+++ templates/pre-header.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+ * Bonnie Souter 03/08/2016
+ * Moved pre-header info from templates to consolidate identical function calls.
+ */
+?>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-M6F94N"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M6F94N');</script>
+<!-- End Google Tag Manager -->
+  <!--[if lt IE 8]>
+    <div class="alert alert-warning">
+      <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
+    </div>
+  <![endif]-->
